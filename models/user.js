d66587cf1dc147b216ea21e7bdef47807f8913f2
Danshendaisy <37489386+Danshendaisy@users.noreply.github.com>
2018-08-01 11:10:10 -0700

    version0.1

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose')
+
+const userSchema = new Schema({
+ 
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+
+let User = mongoose.model('User', userSchema);
+module.exports=User;
---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const MongoStore = require('connect-mongo')(session);
+const app = express();
+const User = require('./models/user.js');
+
+if (!process.env.MONGODB_URI) {
+    console.log('MONGODB_URI config failed');
+    process.exit(1);
+  }
+  mongoose.Promise = global.Promise;
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+  mongoose.connection.on('error', console.error);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.post('/register', (req, res) => {
+    const newUser = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    newUser.save()
+    .then(response => {
+      res.send(response);
+    })
+    .catch(error => {
+      res.send(error);
+    })
+  });
+
+app.post('/login',(req,res) =>{
+  console.log('booty', req.body)
+  User.findOne({username:req.body.username}, function(err, user) {
+          if (err) {
+            console.error(err);
+            res.json({
+              success:false,
+              message: "Error!" + err
+            });
+          }
+          else if (!user) {
+            console.log("user",user);
+            res.json({
+              success:false,
+              message: "Invalid user"
+          });
+          }
+          //if passwords don't match, authorization failed
+          else if (user.password !== req.body.password) {
+            res.json({
+              success: false,
+              message:"Incorrect password"
+            })
+          }
+         else{ console.log({user:req.body})
+          //if authorization succeeds
+          res.json({
+            success: true, 
+            user: user
+          })
+        }
+        });
+ 
+});
+
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+
+const server = app.listen(3000, () => {
+  const { address, port } = server.address();
+  console.log(`Listening at http://${port}`);
+})
+
+
