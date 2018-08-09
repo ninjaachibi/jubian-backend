cd057a902d750c7f6c8705bc9c4a39c509b4d3d1
Robert L <1998.robert@gmail.com>
2018-08-08 17:03:12 -0700

    Reorganize files

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -24,56 +24,78 @@ router.use(function(req, res, next) {
   next();
 });
 
-// fetch('/api/user', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//      'Authorization': 'Bearer ' + localStorage.getItem('token')
-//   },
-//   body: JSON.stringify({
-
-//   })
-// })
-
-//USERORDER
-router.get('/userOrder',(req,res)=>{
-  let userid = req.user._id;
-  console.log('userid',userid);
-  Order.findOne({orderedBy:userid})
-  .then(order =>{
-    console.log(order)
-    res.json({order})
-  })
-  .catch(err =>{
-    console.log(err)
-    res.json({err})
-  })
-})
-
-//ORDER
-router.post('/Order',(req,res) =>{
-    const newOrder = new Order({
-      totalPrice:req.body.totalPrice,
-      orderedBy: req.user._id, //need to change this to client userId
-      address:req.body.address,
-      item:req.body.item
-    })
-  
-    newOrder.save(function(err){
-      if(err){
-        console.log("Error",err)
-      }
-      else{
+//AUTH ROUTES
+router.post('/register', (req, res) => {
+  console.log('body', req.body);
+  User.findOne({username: req.body.username})
+  .then((user) => {
+    console.log('user', user);
+    if(user) {
+      res.json({
+        success: false,
+        message: "Username already exists!"
+      })
+    }
+    else {
+      const newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+      newUser.save()
+      .then(user => {
         res.json({
-          success:true
-        })
-        Order.find({})
-        .populate('orderedBy')
-        .exec(function(error,orders){
-          console.log(JSON.stringify(orders, null, "\t"))
-        })
-      }
-    })
-})
+          success: true,
+          message: `Successfully registered a new user: ${user.username}!`
+        });
+      })
+      .catch(error => {
+        res.json({
+          success: false,
+          message: `Error: ${error}`
+        });
+      })
+    }
+  })
+});
+
+router.post('/login',(req,res) =>{
+  console.log('booty', req.body)
+  User.findOne({username:req.body.username}, function(err, user) {
+    if (err) {
+      console.error(err);
+      res.json({
+        success:false,
+        message: "Error!" + err
+      });
+    }
+    else if (!user) {
+      console.log("user",user);
+      res.json({
+        success:false,
+        message: "Invalid user"
+      });
+    }
+    //if passwords don't match, authorization failed
+    else if (user.password !== req.body.password) {
+      res.json({
+        success: false,
+        message:"Incorrect password"
+      })
+    }
+    else{ console.log({user:req.body})
+    //if authorization succeeds
+    res.json({
+      success: true,
+      userId: user._id,
+      token: jwt.sign(
+        { _id: user.id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: '1d' })
+      })
+    }
+  });
+});
+
+
 
 export default router;
