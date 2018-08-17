c163d4ed3d84e77a2dde86cb20a44c7fd51b8cc4
Danshendaisy <37489386+Danshendaisy@users.noreply.github.com>
2018-08-17 16:21:00 -0700

    Add production build

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const allowCrossDomain = function(req, res,next) {
   res.header('Access-Control-Allow-Origin', 'https://goldenexpress-driver.herokuapp.com/');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 }
 
 app.use(allowCrossDomain)
