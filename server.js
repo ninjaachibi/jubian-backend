698f35bdc3b4796a66592c7915d2c308b3173e50
Danshendaisy <37489386+Danshendaisy@users.noreply.github.com>
2018-08-17 16:27:28 -0700

    Add production build

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connection.on('connected', function() {
 })
 
 const allowCrossDomain = function(req, res,next) {
-  res.header('Access-Control-Allow-Origin', 'https://goldenexpress-driver.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
