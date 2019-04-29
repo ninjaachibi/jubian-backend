871021f9e0358e8cee82f56bac0846375229d552
jku <kujo516@gmail.com>
2019-04-28 22:34:14 -0400

    added different addresses in order schema

--- maps/geocoding.js
+++ maps/geocoding.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
-async function getCoords(rawAddress){
-    let address = rawAddress.split(' ').join('+');
-    console.log('address', address);
+async function getCoords(addObj){
+    let address = addObj.address.split(' ').join('+');
+    let city = addObj.city.split(' ').join('+');
+    let ZIP = addObj.ZIP.split(' ').join('+');
+    let full = address+','+city+','+ZIP
+
+    console.log('address', full);
     // console.log('process.env', process.env);
     let geocode = await axios.get('http://www.mapquestapi.com/geocoding/v1/address', {
             params: {
             key: process.env.API_KEY,
-            location: address,
+            location: full,
             },
         }).then(response => response.data.results[0].locations[0].displayLatLng, 
                 error => console.error(error));
